7fcdf5d3f20e9df79921561658484166a69989a2
Colin Meret <colinmeret@gmail.com>
2014-09-10 01:06:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rotationapp::Application.config.secret_key_base = '4f24cd3f7f8b1f6923331065c92782dedd3208a09a0a2001a423c419a8fe9d41dee175bf8ae2f4665ae990abaa493617326f80e7104842cfe5e2d4873510602e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rotationapp::Application.config.session_store :cookie_store, key: '_rotationapp_session'
